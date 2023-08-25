71fa3293a756b3de04b461752e2de9cc76aacc99
molinxo <jeanmolin16@gmail.com>
2023-08-24 23:52:32 -0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -66,6 +66,17 @@ class Player(pygame.sprite.Sprite):
         self.direction = "left"
         self.animation_count = 0
         self.fall_count = 0
+        self.jump_count = 0
+
+    def jump(self):
+        self.y_vel = self.GRAVITY * 8
+        self.animation_count += 1
+
+        if self.jump_count == 1:
+            self.fall_count = 0
+            
+            
+    
 
     def move(self, dx, dy):
         self.rect.x += dx
