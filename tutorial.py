81dec9623cf1edebf9a6a2d3adfa4ed264f21acd
molinxo <jeanmolin16@gmail.com>
2023-08-25 21:58:19 -0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -177,17 +177,18 @@ def draw(window, background, bg_image, player, objects):
 def handle_vertical_collision(player, objects, dy):
     collided_objects = []
     for obj in objects:
-        if pygame.sprite.collide_mask(player, obj):
-            if dy > 0:
-                player.rect.bottom = obj.rect.top
-                player.landed()
-            elif dy < 0:
-                player.rect.top = obj.rect.bottom
-                player.hit_head()
+        #if pygame.sprite.collide_mask(player, obj):
+        #    if dy > 0:
+        #        player.rect.bottom = obj.rect.top
+        #        player.landed()
+        #    elif dy < 0:
+        #        player.rect.top = obj.rect.bottom
+        #        player.hit_head()
 
-        collided_objects.append(obj)
-        
+            collided_objects.append(obj)
+
     return collided_objects
+
 
 def handle_move(player, objects):
     keys = pygame.key.get_pressed()
